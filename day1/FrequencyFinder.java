cafb7018c687b43903e43a2f576dae96e5966bcb
Kevin Hu <contactKevinHu@gmail.com>
2019-03-04 23:10:04 -0500

    changed to a hashSet since hashMap was overkill

--- day1/FrequencyFinder.java
+++ day1/FrequencyFinder.java
@@ -5,11 +5,11 @@ public class FrequencyFinder{
 	public static void main(String args[]) throws FileNotFoundException {
 		File input = new File("input.txt");
 		Scanner in = new Scanner(new FileInputStream(input));
-		HashMap<Integer, Integer> values = new HashMap<Integer, Integer>();
+		HashSet<Integer> values = new HashSet<Integer>();
 		boolean stop = false;
 		int modifier = 0;
 		int result = 0;
-		values.put(result, 1);
+		values.add(result);
 		while(!stop){
 			if(!in.hasNext()){
 				in = new Scanner(new FileInputStream(input));
@@ -17,12 +17,12 @@ public class FrequencyFinder{
 			else{
 				modifier = in.nextInt();
 				result += modifier;
-				if(values.containsKey(result)){
+				if(values.contains(result)){
 					System.out.println(result);
 					stop = true;
 				}
 				else{
-					values.put(result, 1);
+					values.add(result);
 				}
 			}
 		}
